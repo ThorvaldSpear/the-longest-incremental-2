468c49f03f8d804d45e55c6dc1509c8f106a30e4
circle-gon <circle-gon@users.noreply.github.com>
2022-09-05 16:42:42 +0000

    Minify source code

--- src/features/import-layer.js
+++ src/features/import-layer.js
@@ -1,4 +1,1 @@
-//Layer 0
-import "./0-quarry/inventory.js";
-import "./0-quarry/green-papers.js";
-import "./0-quarry/encyclopedia.js";
+import"./0-quarry/inventory.js";import"./0-quarry/green-papers.js";import"./0-quarry/encyclopedia.js";
